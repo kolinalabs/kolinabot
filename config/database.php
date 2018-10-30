90dc2ba0acdc9368afb6888d5eac9db1003e6c9a
Gianluca Bine <gian_bine@hotmail.com>
2018-10-30 13:33:23 -0300

    Configuração de banco de dados redis para uso em fila de jobs

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the SicesSolar package.
+ *
+ * (c) SicesSolar <http://sicesbrasil.com.br/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'redis' => [
+        'client' => 'predis',
+        'default' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', "0"),
+        ],
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', "1"),
+        ]
+    ]
+];
